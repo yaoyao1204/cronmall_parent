01817ecc5c07b21b6b43343cdc7846f542823579
yao <1585257083@qq.com>
2022-08-10 00:06:27 +0800

    商城项目第一次commit

---
+++ service/service_edu/src/main/java/com/study/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package com.study.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author yao
+ * @since 2022-08-10
+ */
+@RestController
+@RequestMapping("/eduservice/edu-teacher")
+public class EduTeacherController {
+
+}
+
---
+++ service/service_edu/src/main/java/com/study/eduservice/service/EduTeacherService.java
@@ -0,0 +1,16 @@
+package com.study.eduservice.service;
+
+import com.study.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author yao
+ * @since 2022-08-10
+ */
+public interface EduTeacherService extends IService<EduTeacher> {
+
+}
